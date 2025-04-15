ab9a131627deba19a77a2f549f4270a011105a61
Jamkris <dltmdgus1412@gmail.com>
2025-04-15 17:48:32 +0900

    feat(#10) :: Splash Page 제작

--- src/app/splash/page.tsx
+++ src/app/splash/page.tsx
@@ -1,7 +1,12 @@
+import TextLogo from '@/components/TextLogo';
 import React from 'react';
 
 const Splash = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-row items-center justify-center w-full h-full">
+      <TextLogo />
+    </div>
+  );
 };
 
 export default Splash;
