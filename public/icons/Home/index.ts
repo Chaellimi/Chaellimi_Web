8f52f788ef7af7ceeba454ab54f9d2bc3cdb8e1d
Jamkris <dltmdgus1412@gmail.com>
2025-04-16 13:31:34 +0900

    feat(#12) :: export default 묶음

---
+++ public/icons/Home/index.ts
@@ -0,0 +1,3 @@
+export { default as MagnifyingGlassIcon } from './MagnifyingGlass';
+export { default as PointIcon } from './Point';
+export { default as SearchBarCancelIcon } from './SearchBarCancel';
