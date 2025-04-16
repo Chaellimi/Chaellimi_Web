52bedc73043360f72e81eaad964057bc34065ce9
Jamkris <dltmdgus1412@gmail.com>
2025-04-16 20:50:26 +0900

    feat(#12) :: 참여중인 챌린지 재사용 컴포넌트로 제작

---
+++ src/components/Home/ActiveChallenge.tsx
@@ -0,0 +1,54 @@
+import Image from 'next/image';
+import React from 'react';
+
+interface OwnProps {
+  isActive: boolean;
+  progress: number;
+  imgURL: string;
+  time: string;
+  title: string;
+}
+
+const ActiveChallenge = ({
+  isActive,
+  progress,
+  imgURL,
+  time,
+  title,
+}: OwnProps) => {
+  return (
+    <div className="flex flex-col w-[12.5rem] border-[1px] border-gray-100 rounded-lg p-[0.88rem] gap-[0.62rem]">
+      <div
+        className={`text-c1 w-fit pr-[0.38rem] pl-[0.38rem] pt-[0.19rem] pb-[0.19rem] rounded
+          ${isActive ? 'text-primary-default bg-primary-light' : 'text-gray-400 bg-gray-100'}`}
+      >
+        진행률 {progress}%
+      </div>
+
+      <div className="flex items-center gap-[0.62rem] w-full">
+        <div>
+          <Image
+            src={imgURL}
+            width={46}
+            height={46}
+            alt=""
+            className="rounded-xl"
+          />
+        </div>
+        <div>
+          <div className="text-c2">{time}</div>
+          <div className="text-b2">{title}</div>
+        </div>
+      </div>
+
+      <div
+        className={`flex items-center justify-center w-full p-[0.62rem] text-bn3 text-white rounded-lg
+          ${isActive ? 'bg-primary-default' : 'bg-gray-300'}`}
+      >
+        {isActive ? '오늘 인증 완료!' : '인증하러 가기'}
+      </div>
+    </div>
+  );
+};
+
+export default ActiveChallenge;
