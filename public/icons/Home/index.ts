1bde32f3848dde1c44570b9df6847b5a88885444
Jamkris <dltmdgus1412@gmail.com>
2025-04-16 22:28:43 +0900

    feat(#12) :: ArrowIcon, FireIcon 가져오기 추가

--- public/icons/Home/index.ts
+++ public/icons/Home/index.ts
@@ -1,3 +1,5 @@
 export { default as MagnifyingGlassIcon } from './MagnifyingGlass';
 export { default as PointIcon } from './Point';
 export { default as SearchBarCancelIcon } from './SearchBarCancel';
+export { default as ArrowIcon } from './Arrow';
+export { default as FireIcon } from './Fire';
