e2310541ba541eb0bce247971c73b6f7bb714c61
Jamkris <dltmdgus1412@gmail.com>
2025-04-16 13:04:59 +0900

    feat(#12) :: PointIcon 추가

---
+++ public/icons/Point.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const PointIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="12" cy="12" r="11" fill="#FFDF5D" />
+      <path
+        d="M9.0957 17V7.10156H12.8145C15.084 7.10156 16.3008 8.48242 16.3008 10.3828C16.3008 12.2832 15.0703 13.6504 12.7871 13.6504H10.873V17H9.0957ZM10.873 12.1875H12.541C13.8809 12.1875 14.4824 11.4355 14.4824 10.3828C14.4824 9.31641 13.8809 8.5918 12.541 8.5918H10.873V12.1875Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
+
+export default PointIcon;
