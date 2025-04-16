01c853c7a7aaa1fe14df25a92358bfc2819c1998
Jamkris <dltmdgus1412@gmail.com>
2025-04-16 23:58:10 +0900

    feat(#12) :: PC 레이아웃 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import LayoutWithNav from './layoutWithNav';
+
 export const metadata: Metadata = {
   title: 'Chaellimi',
   description: 'Chaellimi',
@@ -13,9 +14,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className="w-[100vw] h-[100vh] overflow-hidden">
-        <div className="flex justify-center w-full h-full bg-primary-light">
-          <div className="w-[430px] min-h-screen bg-white">
+      <body className=" bg-primary-light w-[100vw] h-[100vh] max-h-[100vh] max-w-[100vw] overflow-hidden">
+        <div className="flex justify-center items-cente min-h-screen sm:h-[820px] sm:pl-[39%] sm:pr-[39%] sm:pt-[8%]">
+          <div
+            className="w-full min-h-screen md:h-[200px] bg-white sm:rounded-xl sm:pt-10"
+            style={{
+              boxShadow: '0 0 20px rgba(0, 0, 0, 0.1)',
+            }}
+          >
             <LayoutWithNav>{children}</LayoutWithNav>
           </div>
         </div>
