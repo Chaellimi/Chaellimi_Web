bd152a998a1af52a7bac0231968676a930186653
Jamkris <dltmdgus1412@gmail.com>
2025-04-16 13:05:17 +0900

    feat(#12) :: Home Header 완성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import TextLogo from '@/components/shared/TextLogo';
+import PointIcon from '@public/icons/Point';
 import React from 'react';
 
 const Home = () => {
-  return <div className="w-full h-[100vh]"></div>;
+  return (
+    <div className="w-full h-[100vh]">
+      <header className="flex items-center justify-between w-full h-11 pr-7 pl-7">
+        <TextLogo width={75} />
+        <PointIcon />
+      </header>
+    </div>
+  );
 };
 
 export default Home;
