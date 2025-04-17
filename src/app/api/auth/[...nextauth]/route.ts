8a25207a2e3f0bf002f8412933fa2845b6c78bec
Jamkris <dltmdgus1412@gmail.com>
2025-04-18 08:16:12 +0900

    fix(#14) :: 파일구조 개편

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth';
-import { authOptions } from '@/lib/authOptions';
+import { authOptions } from '@/lib/OAuth/googleAuthOptions';
 
 const handler = NextAuth(authOptions);
 
