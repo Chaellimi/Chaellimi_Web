e27d6a68fac429e48d1fa70c4bdcbdfe9754340b
Jamkris <dltmdgus1412@gmail.com>
2025-04-22 13:58:47 +0900

    feat(#27) :: Check 아이콘 추가

--- public/icons/Challenge/index.ts
+++ public/icons/Challenge/index.ts
@@ -2,3 +2,4 @@ export { default as MagnifyingGlassIcon } from './MagnifyingGlass';
 export { default as PlusIcon } from './Plus';
 export { default as UnderArrowIcon } from './UnderArrow';
 export { default as ResetIcon } from './Reset';
+export { default as CheckIcon } from './Check';
