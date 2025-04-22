4c247def16b15e3817f16f6188160203db5f98b6
Jamkris <dltmdgus1412@gmail.com>
2025-04-22 16:41:51 +0900

    feat(#27) :: Bookmark 아이콘 추가

--- public/icons/Challenge/index.ts
+++ public/icons/Challenge/index.ts
@@ -3,3 +3,4 @@ export { default as PlusIcon } from './Plus';
 export { default as UnderArrowIcon } from './UnderArrow';
 export { default as ResetIcon } from './Reset';
 export { default as CheckIcon } from './Check';
+export { default as BookmarkIcon } from './Bookmark';
