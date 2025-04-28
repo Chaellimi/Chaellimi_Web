43c82e8674e45f5d1b90ada51a025a10e09d74f1
Jamkris <dltmdgus1412@gmail.com>
2025-04-28 17:50:49 +0900

    feat(#35) :: 챌린지 자세히보기로 이동 되도록 제작

--- src/components/Challenge/ChallengeContent.tsx
+++ src/components/Challenge/ChallengeContent.tsx
@@ -1,9 +1,11 @@
 import { BookmarkIcon } from '@public/icons/Challenge';
 import { FireIcon } from '@public/icons/shared';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 interface OwnProps {
+  id: number;
   count: number;
   title: string;
   imgUrl: string;
@@ -14,6 +16,7 @@ interface OwnProps {
 }
 
 const ChallengeContent = ({
+  id,
   imgUrl,
   title,
   count,
@@ -23,23 +26,30 @@ const ChallengeContent = ({
   days,
 }: OwnProps) => {
   return (
-    <div className="flex flex-col gap-2">
-      <div
-        className="w-full aspect-square rounded-xl"
-        style={{
-          backgroundImage: `url(${imgUrl})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      >
-        <div className="flex flex-col justify-between h-full w-full px-[0.66rem] py-2">
-          <div className="flex justify-end w-full">
-            <BookmarkIcon />
-          </div>
+    <Link href={`/challenge/${id}`}>
+      <div className="flex flex-col gap-1">
+        <div
+          className="w-full aspect-square rounded-xl"
+          style={{
+            backgroundImage: `url(${imgUrl})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        >
+          <div className="flex flex-col justify-between h-full w-full px-[0.66rem] py-2">
+            <button
+              className="flex justify-end w-full"
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+              }}
+            >
+              <BookmarkIcon />
+            </button>
 
-          <div className="flex items-center justify-between w-full">
-            <div
-              className={`text-center text-c2 px-[0.38rem] py-[0.15rem] rounded-[0.25rem] w-16
+            <div className="flex items-center justify-between w-full">
+              <div
+                className={`text-center text-c2 px-[0.38rem] py-[0.15rem] rounded-[0.25rem] w-16
                 ${
                   difficulty === 'Hard'
                     ? 'bg-red-100 text-red-200'
@@ -48,39 +58,40 @@ const ChallengeContent = ({
                       : 'bg-green-100 text-green-200'
                 }
                 `}
-            >
-              {difficulty === 'Hard'
-                ? '난이도 상'
-                : difficulty === 'Medium'
-                  ? '난이도 중'
-                  : '난이도 하'}
-            </div>
-            <div className="text-center text-c2 first-line px-[0.38rem] py-[0.15rem] bg-gray-50 rounded-[0.25rem] w-16 text-gray-500">
-              {days}일 도전
+              >
+                {difficulty === 'Hard'
+                  ? '난이도 상'
+                  : difficulty === 'Medium'
+                    ? '난이도 중'
+                    : '난이도 하'}
+              </div>
+              <div className="text-center text-c2 first-line px-[0.38rem] py-[0.15rem] bg-gray-50 rounded-[0.25rem] w-16 text-gray-500">
+                {days}일 도전
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="text-b2">{title}</div>
-      <div className="flex items-center gap-1 text-gray-500 text-b3">
-        <FireIcon />
-        {count}명 도전 중
-      </div>
-
-      <div className="flex items-center gap-[0.31rem]">
-        <div className="relative rounded-full w-9 h-9">
-          <Image
-            src={createrImgUrl}
-            alt={createrName}
-            width={36}
-            height={36}
-            className="object-cover object-top w-full h-full rounded-full"
-          />
+        <div className="text-b2">{title}</div>
+        <div className="flex items-center gap-1 text-gray-500 text-b3">
+          <FireIcon />
+          {count}명 도전 중
         </div>
-        <div>{createrName}</div>
+
+        <div className="flex items-center gap-[0.31rem]">
+          <div className="relative rounded-full w-7 h-7">
+            <Image
+              src={createrImgUrl}
+              alt={createrName}
+              width={28}
+              height={28}
+              className="object-cover object-top w-full h-full rounded-full"
+            />
+          </div>
+          <div>{createrName}</div>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
