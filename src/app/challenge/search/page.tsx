1985cf9c184b5e4016e23fa6c20d1f93cc15d287
Jamkris <dltmdgus1412@gmail.com>
2025-04-28 13:18:02 +0900

    feat(#36) :: 검색페이지 검색헤더 추가

--- src/app/challenge/search/page.tsx
+++ src/app/challenge/search/page.tsx
@@ -1,7 +1,24 @@
+'use client';
+
+import Header from '@/components/shared/Header';
 import React from 'react';
 
 const ChallengeSearch = () => {
-  return <div>Search</div>;
+  const [isSearchbarVisible, setIsSearchbarVisiable] = React.useState(false);
+  const [searchText, setSearchText] = React.useState('');
+
+  return (
+    <div className="flex flex-col w-full h-full text-gray-black">
+      <Header
+        type="search"
+        searchText={searchText}
+        setSearchText={setSearchText}
+        isSearchbarVisible={isSearchbarVisible}
+        setIsSearchbarVisiable={setIsSearchbarVisiable}
+      />
+      <div></div>
+    </div>
+  );
 };
 
 export default ChallengeSearch;
