e7901a80715ed3215bcc9e0c90315abe8cc1fbf5
Jamkris <dltmdgus1412@gmail.com>
2025-04-30 08:20:40 +0900

    feat(#41) :: ShareIcon 추가

--- public/icons/shared/index.ts
+++ public/icons/shared/index.ts
@@ -2,3 +2,4 @@ export { default as MagnifyingGlassIcon } from './MagnifyingGlass';
 export { default as SearchBarCancelIcon } from './SearchBarCancel';
 export { default as FireIcon } from './Fire';
 export { default as ArrowIcon } from './Arrow';
+export { default as ShareIcon } from './Share';
