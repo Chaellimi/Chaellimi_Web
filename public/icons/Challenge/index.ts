990ae45fa14f8d8eb4c4ececccce65cb39aa443a
Jamkris <dltmdgus1412@gmail.com>
2025-05-12 07:47:18 +0900

    fix(#46) :: 아이콘 폴더 구조수정

--- public/icons/Challenge/index.ts
+++ public/icons/Challenge/index.ts
@@ -2,6 +2,3 @@ export { default as PlusIcon } from './Plus';
 export { default as ResetIcon } from './Reset';
 export { default as CheckIcon } from './Check';
 export { default as FilterCheckIcon } from './FilterCheck';
-export { default as ClockIcon } from './Clock';
-export { default as CancelIocn } from './Cancel';
-export { default as MoreVerticalDotIcon } from './MoreVerticalDot';
--- src/app/challenge/write/page.tsx
+++ src/app/challenge/write/page.tsx
@@ -1,7 +1,13 @@
+import Header from '@/components/shared/Header';
+import { CancelIcon } from '@public/icons/Challenge/write';
 import React from 'react';
 
 const Write = () => {
-  return <div>dfasdasdf</div>;
+  return (
+    <div>
+      <Header type="default" title="챌린지 생성" backIcon={<CancelIcon />} />
+    </div>
+  );
 };
 
 export default Write;
