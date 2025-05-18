c39ce23564e152357653accfd4a0ef5ebe789ed8
Jamkris <dltmdgus1412@gmail.com>
2025-05-19 08:42:04 +0900

    feat(#53) :: PointIcon 추가

---
+++ public/icons/Challenge/point/Point.tsx
@@ -0,0 +1,280 @@
+import React from 'react';
+
+const PointIcon = () => {
+  return (
+    <svg
+      width="46"
+      height="46"
+      viewBox="0 0 46 46"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_d_1004_6724)">
+        <g filter="url(#filter1_i_1004_6724)">
+          <path
+            d="M43 23C43 34.0457 34.0457 43 23 43C11.9543 43 3 34.0457 3 23C3 11.9543 11.9543 3 23 3C34.0457 3 43 11.9543 43 23Z"
+            fill="url(#paint0_linear_1004_6724)"
+          />
+        </g>
+        <path
+          d="M23 4.5332C33.1989 4.5332 41.4668 12.8011 41.4668 23C41.4668 33.1989 33.1989 41.4668 23 41.4668C12.8011 41.4668 4.5332 33.1989 4.5332 23C4.5332 12.8011 12.8011 4.5332 23 4.5332Z"
+          stroke="url(#paint1_linear_1004_6724)"
+          strokeWidth="3.06667"
+        />
+      </g>
+      <g clipPath="url(#clip0_1004_6724)">
+        <path
+          d="M29.3808 15.8339C29.2917 15.8814 29.197 15.9319 29.0978 15.985C29.0686 15.6791 28.8486 15.2632 28.735 15.0292C28.4288 14.3983 27.9092 13.4402 27.8244 13.2679C26.0692 9.70021 20.9164 10.8034 21.2634 15.0998C21.2653 15.1234 21.2366 15.1384 21.2198 15.1216L20.7301 14.6345C18.1792 11.5633 13.2233 14.2065 14.9046 18.5963C14.9129 18.618 14.8897 18.64 14.8688 18.6297C11.9234 17.188 9.13063 18.8912 9.58156 22.3765C9.71011 23.3741 10.0509 24.4307 10.4775 25.3685C11.9615 27.9282 13.1153 30.2892 15.6452 32.2218C25.1413 39.4765 38.9999 30.9163 35.7042 19.4648C34.7496 16.1541 33.141 14.6895 29.3808 15.8339Z"
+          fill="url(#paint2_linear_1004_6724)"
+        />
+        <path
+          d="M29.1026 28.0752C30.3708 28.0752 31.3988 27.0527 31.3988 25.7914C31.3988 24.5301 30.3708 23.5076 29.1026 23.5076C27.8344 23.5076 26.8064 24.5301 26.8064 25.7914C26.8064 27.0527 27.8344 28.0752 29.1026 28.0752Z"
+          fill="#FFE141"
+          fillOpacity="0.5"
+        />
+        <path
+          d="M26.1396 22.1066C27.0397 22.0194 27.8751 22.8754 27.9892 24.0408C28.1033 25.2061 27.4497 26.2064 26.5496 26.2936C25.6496 26.3808 24.8142 25.5247 24.7 24.3595C24.5858 23.1941 25.2395 22.1939 26.1396 22.1066Z"
+          fill="white"
+          stroke="#FFD600"
+          strokeWidth="0.08"
+        />
+        <path
+          d="M26.4017 25.5566C27.1667 25.5682 27.7983 24.8237 27.8124 23.8937C27.8266 22.9638 27.2179 22.2006 26.453 22.1891C25.688 22.1776 25.0564 22.9221 25.0422 23.852C25.0281 24.7819 25.6367 25.5451 26.4017 25.5566Z"
+          fill="#FFD600"
+        />
+        <path
+          d="M27.8168 23.5985C27.8074 24.212 27.3666 24.425 26.8269 24.4169C26.2872 24.4087 25.8573 23.9048 25.8666 23.2914C25.8759 22.6779 26.1814 22.1851 26.7211 22.1932C27.2608 22.2014 27.8261 22.985 27.8168 23.5985Z"
+          fill="white"
+        />
+        <path
+          opacity="0.4"
+          d="M27.174 23.2031L25.9958 25.6541"
+          stroke="white"
+          strokeWidth="1.10092"
+        />
+        <path
+          d="M26.7919 25.1032C26.6644 25.1013 26.5627 24.9969 26.5646 24.8701C26.5665 24.7433 26.6714 24.6421 26.7989 24.644C26.9264 24.6459 27.0282 24.7503 27.0263 24.8771C27.0244 25.0039 26.9194 25.1051 26.7919 25.1032Z"
+          fill="white"
+        />
+        <path
+          d="M25.8293 21.0211L26.8063 20.97"
+          stroke="#FFD600"
+          strokeWidth="0.733945"
+          strokeLinecap="round"
+        />
+        <path
+          d="M21.7036 27.314C22.2351 27.6969 23.7447 28.2331 24.2549 27.314"
+          stroke="#FFD600"
+          strokeWidth="2.20183"
+          strokeLinecap="round"
+        />
+        <path
+          opacity="0.9"
+          d="M21.7036 27.314C22.2351 27.6969 23.7447 28.2331 24.2549 27.314"
+          stroke="white"
+          strokeWidth="0.366973"
+          strokeLinecap="round"
+        />
+        <path
+          d="M17.3663 28.0752C18.6344 28.0752 19.6625 27.0527 19.6625 25.7914C19.6625 24.5301 18.6344 23.5076 17.3663 23.5076C16.0981 23.5076 15.0701 24.5301 15.0701 25.7914C15.0701 27.0527 16.0981 28.0752 17.3663 28.0752Z"
+          fill="#FFE141"
+          fillOpacity="0.5"
+        />
+        <path
+          d="M22.8518 25.5376C22.9794 25.6645 23.1325 25.6019 23.2345 25.5376"
+          stroke="#FFE141"
+          strokeWidth="0.513761"
+          strokeLinecap="round"
+        />
+        <path
+          d="M19.63 22.1066C20.5302 22.0194 21.3656 22.8754 21.4797 24.0408C21.5937 25.2061 20.9402 26.2064 20.0401 26.2936C19.1401 26.3808 18.3046 25.5247 18.1904 24.3595C18.0763 23.1941 18.73 22.1939 19.63 22.1066Z"
+          fill="white"
+          stroke="#FFD600"
+          strokeWidth="0.08"
+        />
+        <path
+          d="M19.8922 25.5566C20.6571 25.5682 21.2888 24.8237 21.3029 23.8937C21.3171 22.9638 20.7084 22.2006 19.9435 22.1891C19.1785 22.1776 18.5469 22.9221 18.5327 23.852C18.5185 24.7819 19.1272 25.5451 19.8922 25.5566Z"
+          fill="#FFD600"
+        />
+        <path
+          d="M21.3072 23.5985C21.2979 24.212 20.857 24.425 20.3174 24.4169C19.7777 24.4087 19.3477 23.9048 19.3571 23.2914C19.3664 22.6779 19.6719 22.1851 20.2116 22.1932C20.7513 22.2014 21.3166 22.985 21.3072 23.5985Z"
+          fill="white"
+        />
+        <path
+          opacity="0.4"
+          d="M20.6644 23.2031L19.4863 25.6541"
+          stroke="white"
+          strokeWidth="1.10092"
+        />
+        <path
+          d="M20.2824 25.1032C20.1549 25.1013 20.0531 24.9969 20.0551 24.8701C20.057 24.7433 20.1619 24.6421 20.2894 24.644C20.4169 24.6459 20.5187 24.7503 20.5168 24.8771C20.5148 25.0039 20.4099 25.1051 20.2824 25.1032Z"
+          fill="white"
+        />
+        <path
+          d="M19.1523 20.7163L19.9391 21.0413"
+          stroke="#FFD600"
+          strokeWidth="0.733945"
+          strokeLinecap="round"
+        />
+        <path
+          opacity="0.2"
+          d="M16.7409 25.2571L16.5261 26.2957"
+          stroke="white"
+          strokeWidth="0.366973"
+          strokeLinecap="round"
+        />
+        <path
+          opacity="0.2"
+          d="M17.3711 25.5088L17.2388 26.3056"
+          stroke="white"
+          strokeWidth="0.366973"
+          strokeLinecap="round"
+        />
+        <path
+          opacity="0.2"
+          d="M16.0907 25.2905L15.9631 25.9249"
+          stroke="white"
+          strokeWidth="0.366973"
+          strokeLinecap="round"
+        />
+        <path
+          opacity="0.2"
+          d="M29.6052 25.1931L29.3904 26.2317"
+          stroke="white"
+          strokeWidth="0.366973"
+          strokeLinecap="round"
+        />
+        <path
+          opacity="0.2"
+          d="M30.2356 25.4451L30.1033 26.2418"
+          stroke="white"
+          strokeWidth="0.366973"
+          strokeLinecap="round"
+        />
+        <path
+          opacity="0.2"
+          d="M28.9552 25.2268L28.8276 25.8612"
+          stroke="white"
+          strokeWidth="0.366973"
+          strokeLinecap="round"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_1004_6724"
+          x="0.386667"
+          y="0.386667"
+          width="45.2267"
+          height="45.2267"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset />
+          <feGaussianBlur stdDeviation="1.30667" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.403377 0 0 0 0 0.242535 0 0 0 0 0.0816935 0 0 0 0.2 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_1004_6724"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_1004_6724"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter1_i_1004_6724"
+          x="3"
+          y="3"
+          width="44.2623"
+          height="44.518"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="4.2623" dy="4.68853" />
+          <feGaussianBlur stdDeviation="2.25902" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.756648 0 0 0 0 0.55903 0 0 0 0 0.175421 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect1_innerShadow_1004_6724"
+          />
+        </filter>
+        <linearGradient
+          id="paint0_linear_1004_6724"
+          x1="9.97248"
+          y1="7.58716"
+          x2="35.4771"
+          y2="38.4128"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFEF63" />
+          <stop offset="1" stopColor="#FFC908" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_1004_6724"
+          x1="12.728"
+          y1="6.11475"
+          x2="33.4887"
+          y2="40.0098"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFEC89" />
+          <stop offset="1" stopColor="#FFE666" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_1004_6724"
+          x1="9.53345"
+          y1="11.1333"
+          x2="36.2414"
+          y2="37.1095"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="white" />
+          <stop offset="0.701923" stopColor="#FFFAD3" />
+          <stop offset="1" stopColor="#FFF7A9" />
+        </linearGradient>
+        <clipPath id="clip0_1004_6724">
+          <rect
+            width="26.789"
+            height="23.8532"
+            fill="white"
+            transform="translate(9.60547 11.0735)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default PointIcon;
