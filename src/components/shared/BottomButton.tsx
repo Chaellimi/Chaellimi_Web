5399f4d324199819e8e2cdcf7390302000763272
Jamkris <dltmdgus1412@gmail.com>
2025-05-19 10:24:24 +0900

    feat(#50) :: BottomButtom 컴포넌트화

---
+++ src/components/shared/BottomButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface OwnProps {
+  title: string;
+  onClick: () => void;
+  disabled: 'true' | 'false' | 'progress';
+}
+
+const BottomButton = ({ title, onClick, disabled }: OwnProps) => {
+  return (
+    <button
+      className={`flex items-center justify-center w-full h-[3.25rem] rounded-xl text-bn1
+        ${
+          disabled === 'true'
+            ? 'bg-gray-100 text-gray-600'
+            : disabled === 'false'
+              ? 'bg-primary-default text-gray-white'
+              : disabled === 'progress'
+                ? 'bg-primary-default text-gray-white'
+                : null
+        }`}
+      onClick={onClick}
+      disabled={disabled === 'false' || 'progress' ? false : true}
+    >
+      {disabled === 'progress' ? '진행중' : title}
+    </button>
+  );
+};
+
+export default BottomButton;
