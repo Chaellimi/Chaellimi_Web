b254b726d9b9129c64cd1a212dc37a1ce5aa65f3
Jamkris <dltmdgus1412@gmail.com>
2025-05-19 10:38:20 +0900

    feat(#50) :: 참여완료 페이지 퍼블리싱 완료

--- src/app/challenge/finish/page.tsx
+++ src/app/challenge/finish/page.tsx
@@ -1,7 +1,38 @@
+'use client';
+
+import BottomButton from '@/components/shared/BottomButton';
+import Image from 'next/image';
 import React from 'react';
+import { useRouter } from 'next/navigation';
+import FinishCheckImage from '@public/images/FinishCheckImage.png';
 
 const Finish = () => {
-  return <div></div>;
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-col w-full h-full px-6">
+      <div className="flex flex-col items-center justify-center w-full h-full gap-5">
+        <div>
+          <Image src={FinishCheckImage} width={90} alt="" />
+        </div>
+
+        <div className="flex flex-col items-center justify-center gap-[0.31rem]">
+          <div className="text-h1 text-gray-black">참여 완료!</div>
+          <div className="text-gray-500 text-b3">
+            매일 목표를 달성하며 조금씩 성장해보세요.
+          </div>
+        </div>
+      </div>
+
+      <BottomButton
+        title="확인"
+        onClick={() => {
+          router.back();
+        }}
+        disabled="false"
+      />
+    </div>
+  );
 };
 
 export default Finish;
