a4d212302485e2226933405a1430c7ecf5f9487e
Jamkris <dltmdgus1412@gmail.com>
2025-05-21 10:03:40 +0900

    feat(#61) :: Auth Middleware 제작
    로그인 여부를 확인하기 위한 Auth Middleware 제작

---
+++ src/lib/middleware/withAuth.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/lib/OAuth/googleAuthOptions';
+import resUtil from '../utils/responseUtil';
+
+export function withAuth(handler: (req: NextRequest) => Promise<NextResponse>) {
+  return async function (req: NextRequest) {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return NextResponse.json(
+        resUtil.successFalse({
+          status: 401,
+          message: 'Unauthorized',
+          data: { session },
+        })
+      );
+    }
+
+    return handler(req);
+  };
+}
