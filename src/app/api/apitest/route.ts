462e26dc3f0ffc54ba0abc1e45f2b392176fbd82
Jamkris <dltmdgus1412@gmail.com>
2025-05-21 11:10:51 +0900

    feat(#61) :: Test API 제작

---
+++ src/app/api/apitest/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server';
+import resUtil from '@/lib/utils/responseUtil';
+import { withAuth } from '@/lib/middleware/withAuth';
+import { withLogging } from '@/lib/middleware/withLogging';
+
+async function handler() {
+  try {
+    return NextResponse.json(
+      resUtil.successTrue({
+        status: 200,
+        message: 'API Test Success',
+        data: {},
+      })
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(resUtil.unknownError);
+  }
+}
+
+export const GET = withLogging(withAuth(handler));
