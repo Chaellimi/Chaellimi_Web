8af2ef001c74caf18478583b1585106d30a197cc
Jamkris <dltmdgus1412@gmail.com>
2025-05-22 11:39:07 +0900

    feat(#61) :: 챌린지 가져오기 API 제작 완료

--- src/app/api/challenge/route.ts
+++ src/app/api/challenge/route.ts
@@ -1,3 +1,5 @@
 import { CreateChallenge } from './controllers/createChallenge';
-
 export const POST = CreateChallenge;
+
+import { GetChallenge } from './controllers/getChallenge';
+export const GET = GetChallenge;
