0ed3047903461fd9ae362dbad66f369b381a6e21
Jamkris <dltmdgus1412@gmail.com>
2025-06-11 08:02:28 +0900

    feat(#63) :: 토탈포인트 가져오는 API 제작 완료

---
+++ src/app/api/point/controllers/getPoint.ts
@@ -0,0 +1,26 @@
+import Point from '@/database/models/Point';
+import { withAuth } from '@/lib/middleware/withAuth';
+import { withLogging } from '@/lib/middleware/withLogging';
+import getUserFromRequest from '@/lib/utils/getUserFromRequest';
+import resUtil from '@/lib/utils/responseUtil';
+import { NextResponse } from 'next/server';
+
+async function getHandler(): Promise<NextResponse> {
+  const user = await getUserFromRequest();
+  try {
+    const point = await Point.findOne({
+      where: { userId: user?.id },
+      attributes: ['totalPoint'],
+    });
+    console.log(point);
+    return resUtil.successTrue({
+      message: 'Point retrieved successfully',
+      data: point || { totalPoint: 0 },
+    });
+  } catch (error) {
+    console.error(error);
+    return resUtil.unknownError({});
+  }
+}
+
+export const GetPoint = withLogging(withAuth(getHandler));
