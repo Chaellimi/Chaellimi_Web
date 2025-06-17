ebd8c831aadce21ce08063514f7086ef5b180183
Jamkris <dltmdgus1412@gmail.com>
2025-06-17 15:49:03 +0900

    chore(#69) :: 챌린지 및 필터 타입 파일분리

---
+++ src/types/Challenge.ts
@@ -0,0 +1,26 @@
+export interface ChallengFilter {
+  dayStart?: string;
+  dayEnd?: string;
+  category?: string;
+  difficulty?: 'easy' | 'normal' | 'hard';
+  page?: number;
+  size?: number;
+}
+
+export interface ChallengeType {
+  id: number;
+  userId: number;
+  title: string;
+  description: string;
+  category: string;
+  difficulty: 'hard' | 'normal' | 'easy';
+  day: string;
+  imgURL: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string | null;
+  User: {
+    name: string;
+    profileImg: string;
+  };
+}
