0ef3a1751c010ddf90b8b83c2f95d396875cba79
Jamkris <dltmdgus1412@gmail.com>
2025-06-20 08:26:05 +0900

    fix(#69) :: Over STRING 수정

--- src/database/models/Challenge.ts
+++ src/database/models/Challenge.ts
@@ -30,7 +30,7 @@ const Challenge = sequelize.define<ChallengeModel>(
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     userId: { type: DataTypes.INTEGER, allowNull: false },
     title: { type: DataTypes.STRING, allowNull: false },
-    description: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.TEXT, allowNull: false },
     category: { type: DataTypes.STRING, allowNull: false },
     difficulty: {
       type: DataTypes.ENUM('hard', 'normal', 'easy'),
