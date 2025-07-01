d1ab1731b7d591248e7635f87b0101bcea6922dd
Jamkris <dltmdgus1412@gmail.com>
2025-07-01 17:50:52 +0900

    feat(#71) :: 인증부 디자인 맞춰 다시 퍼블리싱

--- src/app/challenge/[id]/certification/done/page.tsx
+++ src/app/challenge/[id]/certification/done/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 
 import React from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { ShapeQuestionIcon } from '@public/icons/Challenge/certification';
 import Header from '@/components/shared/Header';
 import BottomButton from '@/components/shared/BottomButton';
 import Image from 'next/image';
 
 const CertificationDone = () => {
+  const router = useRouter();
   const searchParams = useSearchParams();
   const image = searchParams.get('image');
   const challengeId = usePathname().split('/').slice(0, 4)[2];
@@ -24,24 +25,33 @@ const CertificationDone = () => {
     <div className="flex flex-col items-center w-full h-screen bg-white">
       <Header
         type="default"
+        title="인증하기"
         backClick={`/challenge/${challengeId}/certification`}
         icon={<ShapeQuestionIcon color="black" />}
       />
 
       <div className="flex flex-col items-center justify-center w-full h-full px-4">
-        <h1 className="mb-4 text-gray-800 text-h3">이 사진으로 인증할까요?</h1>
-        <Image
-          width={327}
-          height={327}
-          src={image}
-          alt="인증 사진"
-          className="rounded-lg shadow-md"
-        />
+        <h1 className="mb-4 text-h3">이 사진으로 인증할까요?</h1>
+        <div className="relative h-[327px] w-[327px]">
+          <Image
+            width={327}
+            height={327}
+            src={image}
+            alt="인증 사진"
+            className="object-cover object-top w-full h-full shadow-md rounded-2xl"
+          />
+        </div>
       </div>
 
       <div className="flex items-center justify-center w-full h-16 gap-4 px-6 pt-3 border-t bg-gray-white border-gray-50 custom601:mb-6">
-        <div></div>
-        <div className="h-7 w-[0.0625rem] bg-gray-200" />
+        <button
+          className="h-full text-gray-600 bg-gray-100 min-w-20 rounded-xl"
+          onClick={() => {
+            router.push(`/challenge/${challengeId}/certification`);
+          }}
+        >
+          취소
+        </button>
         <BottomButton title="인증하기" onClick={() => {}} disabled="false" />
       </div>
     </div>
