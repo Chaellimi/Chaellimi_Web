f8315d75b7cb83bee01ae93e9d6f668789f93d7a
Jamkris <dltmdgus1412@gmail.com>
2025-07-01 15:36:15 +0900

    feat(#71) :: GoogleOAuth 권한 문제로 인해 KakaoOAuth 추가

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,5 +1,6 @@
 import { AuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import KakaoProvider from 'next-auth/providers/kakao';
 import Users from '@/database/models/User';
 
 export const authOptions: AuthOptions = {
@@ -8,31 +9,30 @@ export const authOptions: AuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID as string,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: 'jwt',
     maxAge: 30 * 24 * 60 * 60,
   },
-
   callbacks: {
     async signIn({ user, account }) {
       if (!user || !account) return false;
-
       const { email, name, image } = user;
       const authId = account.providerAccountId;
       const provider = account.provider;
-
       try {
         const existingUser = await Users.findOne({
           where: { authId, provider },
         });
-
         if (!email) {
           console.error('User email is missing from OAuth provider');
           return false;
         }
-
         if (!existingUser) {
           await Users.create({
             authId,
@@ -43,19 +43,16 @@ export const authOptions: AuthOptions = {
             provider,
           });
         }
-
         return true;
       } catch (err) {
         console.error('Error during signIn:', err);
         return false;
       }
     },
-
     async jwt({ token, account, user }) {
       if (account) {
         token.accessToken = account.access_token;
       }
-
       if (user?.email) {
         const dbUser = await Users.findOne({ where: { email: user.email } });
         if (dbUser) {
@@ -64,10 +61,8 @@ export const authOptions: AuthOptions = {
           token.picture = dbUser.profileImg;
         }
       }
-
       return token;
     },
-
     async session({ session, token }) {
       session.user = {
         userId: token.userId as number,
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth';
-import { authOptions } from '@/lib/OAuth/googleAuthOptions';
+import { authOptions } from './authOptions';
 import '@/database/index';
 
 const handler = NextAuth(authOptions);
