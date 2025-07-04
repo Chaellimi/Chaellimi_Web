b01b38d15b3512201d5ca4daeb031e1eddb969fa
Jamkris <dltmdgus1412@gmail.com>
2025-07-04 19:55:05 +0900

    fix(#71) :: Loading 컴포넌트 Z-Index 수정

--- src/components/shared/Loading.tsx
+++ src/components/shared/Loading.tsx
@@ -9,7 +9,7 @@ interface LoadingProps {
 
 const Loading = ({ title = '', subTitle = '' }: LoadingProps) => {
   return (
-    <div className="absolute top-0 left-0 z-50 flex flex-col items-center justify-center w-full h-full gap-12 bg-white">
+    <div className="absolute top-0 left-0 z-[200] flex flex-col items-center justify-center w-full h-full gap-12 bg-white">
       <div className="text-h3">{title}</div>
       <div className="animate-spinSlow">
         <SpinLogo />
