57ab8fc484e62484d6b1c0fe58350d89f53ebf67
Jamkris <dltmdgus1412@gmail.com>
2025-07-04 19:57:46 +0900

    feat(#71) :: 인증성공 시 포인트 지급 모달이 2초간 부드럽게 떴다가 사라지게 제작

--- src/app/(private)/challenge/[id]/certification/done/success/page.tsx
+++ src/app/(private)/challenge/[id]/certification/done/success/page.tsx
@@ -6,7 +6,7 @@ import {
   PointModalCheckIcon,
   ShapeQuestionIcon,
 } from '@public/icons/Challenge/certification';
-import { usePathname } from 'next/navigation';
+import { usePathname, useSearchParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 import RangeSlider from 'react-range-slider-input';
 import 'react-range-slider-input/dist/style.css';
@@ -15,10 +15,13 @@ import Image from 'next/image';
 import { formatExifDate } from '@/lib/utils/formatExifDate';
 import { CoinIcon } from '@public/icons/Challenge/progress';
 import BottomButton from '@/components/shared/BottomButton';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const CertificationSuccess = () => {
   const path = usePathname();
   const challengeId = path.split('/').slice(0, 4)[2];
+  const searchParams = useSearchParams();
+  const image = searchParams.get('image');
 
   const [takenTime, setTakenTime] = useState<string | null>(null);
 
@@ -27,11 +30,35 @@ const CertificationSuccess = () => {
     if (time) setTakenTime(time);
   }, []);
 
+  const [showPointToast, setShowPointToast] = useState(false);
+  useEffect(() => {
+    const fadeInTimeout = setTimeout(() => {
+      setShowPointToast(true);
+    }, 500);
+
+    const fadeOutTimeout = setTimeout(() => {
+      setShowPointToast(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(fadeInTimeout);
+      clearTimeout(fadeOutTimeout);
+    };
+  }, []);
+
   useStatusBarBridge({
     backgroundColor: '#EBF5FF',
     translucent: true,
     bottomBackgroundColor: '#EBF5FF',
   });
+
+  if (!image) {
+    return (
+      <div className="flex items-center justify-center w-full h-screen text-lg text-gray-500">
+        이미지 데이터가 없습니다.
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col items-center justify-between w-full h-full overflow-y-auto bg-blue-100">
@@ -74,9 +101,7 @@ const CertificationSuccess = () => {
               <Image
                 width={300}
                 height={300}
-                src={
-                  'https://img.freepik.com/free-photo/man-jump-through-gap-hill-man-jumping-cliff-blue-sky-business-concept-idea_1323-185.jpg?semt=ais_hybrid&w=740'
-                }
+                src={image}
                 alt="인증 사진"
                 className="object-cover object-top w-full h-full shadow-md rounded-2xl"
               />
@@ -97,10 +122,21 @@ const CertificationSuccess = () => {
         </div>
 
         <div className="flex flex-col items-center w-full">
-          <div className="flex items-center justify-center gap-[0.31rem] px-6 py-[0.62rem] bg-gray-600 w-fit rounded-full text-white text-bn3">
-            <PointModalCheckIcon />
-            <div>2000 포인트가 적립되었어요!</div>
-          </div>
+          <AnimatePresence>
+            {showPointToast && (
+              <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 20 }}
+                transition={{ duration: 0.5 }}
+                className="flex items-center justify-center gap-[0.31rem] px-6 py-[0.62rem] bg-gray-600 w-fit rounded-full text-white text-bn3"
+              >
+                <PointModalCheckIcon />
+                <div>2000 포인트가 적립되었어요!</div>
+              </motion.div>
+            )}
+          </AnimatePresence>
+
           <div className="flex items-center justify-center w-full h-16 gap-4 px-6 pt-3 bg-blue-100 border-t border-gray-50 custom601:mb-6">
             <BottomButton title="홈으로" onClick={() => {}} disabled="false" />
           </div>
