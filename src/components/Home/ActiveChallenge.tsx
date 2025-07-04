a684fbf25ca7bd9b2b644e3793eaca18d74ebf10
Jamkris <dltmdgus1412@gmail.com>
2025-07-04 19:56:15 +0900

    fix(#71) :: 홈에서 인증하기 부분 버튼 컴포넌트로 수정
    - 이전에는 컴포넌트 전체를 클릭해도 인증페이지로 넘어갔었지만 이젠 버튼을 클릭해야 넘어가지게 수정
    - 인증 완료한 챌린지의 경우 버튼 disabled 처리

--- src/components/Home/ActiveChallenge.tsx
+++ src/components/Home/ActiveChallenge.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 interface OwnProps {
@@ -20,40 +23,45 @@ const ActiveChallenge = ({
   title,
   link,
 }: OwnProps) => {
+  const router = useRouter();
+
   return (
-    <Link href={link} className="no-underline">
-      <div className="flex flex-col w-[12.5rem] border-[1px] border-gray-100 rounded-lg p-[0.88rem] gap-[0.62rem] min-w-[12.5rem] max-w-[12.5rem]">
-        <div
-          className={`text-c1 w-fit pr-[0.38rem] pl-[0.38rem] pt-[0.19rem] pb-[0.19rem] rounded
+    <div className="flex flex-col w-[12.5rem] border-[1px] border-gray-100 rounded-lg p-[0.88rem] gap-[0.62rem] min-w-[12.5rem] max-w-[12.5rem]">
+      <div
+        className={`text-c1 w-fit pr-[0.38rem] pl-[0.38rem] pt-[0.19rem] pb-[0.19rem] rounded
           ${isActive ? 'text-gray-400 bg-gray-100' : 'text-primary-default bg-primary-light'}`}
-        >
-          진행률 {progress}%
-        </div>
+      >
+        진행률 {progress}%
+      </div>
 
-        <div className="flex items-center gap-[0.62rem] w-full">
-          <div className="w-[2.875rem] h-[2.875rem] relative">
-            <Image
-              src={imgURL}
-              alt=""
-              width={46}
-              height={46}
-              className="object-cover object-top w-full h-full rounded-xl"
-            />
-          </div>
-          <div>
-            {/* <div className="text-c2 text-gray-black">{time}</div> */}
-            <div className="text-b2 text-gray-black">{title}</div>
-          </div>
+      <div className="flex items-center gap-[0.62rem] w-full">
+        <div className="w-[2.875rem] h-[2.875rem] relative">
+          <Image
+            src={imgURL}
+            alt=""
+            width={46}
+            height={46}
+            className="object-cover object-top w-full h-full rounded-xl"
+          />
         </div>
-
-        <div
-          className={`flex items-center justify-center w-full p-[0.62rem] text-bn3 text-white rounded-lg
-          ${isActive ? 'bg-gray-300' : 'bg-primary-default'}`}
-        >
-          {isActive ? '오늘 인증 완료!' : '인증하러 가기'}
+        <div>
+          {/* <div className="text-c2 text-gray-black">{time}</div> */}
+          <div className="text-b2 text-gray-black">{title}</div>
         </div>
       </div>
-    </Link>
+
+      <button
+        className={`flex items-center justify-center w-full p-[0.62rem] text-bn3 text-white rounded-lg
+          ${isActive ? 'bg-gray-300' : 'bg-primary-default'}`}
+        onClick={() => {
+          if (!isActive) {
+            router.push(link);
+          }
+        }}
+      >
+        {isActive ? '오늘 인증 완료!' : '인증하러 가기'}
+      </button>
+    </div>
   );
 };
 
