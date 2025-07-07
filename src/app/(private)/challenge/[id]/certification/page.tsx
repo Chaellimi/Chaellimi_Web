1e63e7aceb289593508507d5411ca085f4022100
Jamkris <dltmdgus1412@gmail.com>
2025-07-07 15:11:11 +0900

    fix(#77) :: 웹환경에서는 인증이 불가능하게 제작

--- src/app/(private)/challenge/[id]/certification/page.tsx
+++ src/app/(private)/challenge/[id]/certification/page.tsx
@@ -16,6 +16,7 @@ import { useEffect, useState } from 'react';
 import EXIF from 'exif-js';
 import useStatusBarBridge from '@/lib/hooks/useStatusBarBridge';
 import PathUtil from '@/lib/utils/pathUtil';
+import isWebEnvironment from '@/lib/utils/isWebEnvironment';
 
 const WrongCertificationExample = [
   {
@@ -64,9 +65,13 @@ const Certification = () => {
 
   // 네이티브 카메라 요청
   const handleNativeCamera = () => {
-    window.ReactNativeWebView?.postMessage(
-      JSON.stringify({ type: 'OPEN_CAMERA' })
-    );
+    if (isWebEnvironment()) {
+      return alert('웹 환경에서는 인증 기능을 사용할 수 없습니다.');
+    } else {
+      window.ReactNativeWebView?.postMessage(
+        JSON.stringify({ type: 'OPEN_CAMERA' })
+      );
+    }
   };
 
   useEffect(() => {
