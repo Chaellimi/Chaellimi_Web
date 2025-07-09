64355264cd3010fd0ea7a8d209aef757318b85ef
Jamkris <dltmdgus1412@gmail.com>
2025-07-09 09:46:04 +0900

    fix(#80) :: Import 네이밍 컨벤션 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import LayoutWithNav from './layoutWithNav';
 import { AuthProviders, ReactQueryProvider } from './providers';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import GoogleAnalytics from '@/lib/utils/googleAnalytics';
+import { GoogleAnalytics } from '@/lib/utils/googleAnalytics';
 
 export const metadata: Metadata = {
   title: 'Chaellimi',
