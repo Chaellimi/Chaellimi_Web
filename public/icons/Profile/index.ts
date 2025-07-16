14d7fedaf66b0012be49455048b80d446c67eb7e
Jamkris <dltmdgus1412@gmail.com>
2025-07-17 07:41:38 +0900

    feat(#83) :: BookmarkIcon 추가

--- public/icons/Profile/index.ts
+++ public/icons/Profile/index.ts
@@ -2,3 +2,4 @@ export { default as AlarmIcon } from './Alarm';
 export { default as CameraIcon } from './Camera';
 export { default as ShopIcon } from './Shop';
 export { default as PointIcon } from './Point';
+export { default as BookmarkIcon } from './Bookmark';
