93cf8516b3200578410f1a9fd38d1127665ee1a2
Jamkris <dltmdgus1412@gmail.com>
2025-07-16 20:14:36 +0900

    feat(#79) :: usePostBuyProduct 상품 구매 제작 완료

--- src/app/(private)/point/shop/[id]/page.tsx
+++ src/app/(private)/point/shop/[id]/page.tsx
@@ -5,22 +5,43 @@ import Header from '@/components/shared/Header';
 import Loading from '@/components/shared/Loading';
 import SelectModal from '@/components/shared/SelectModal';
 import PathUtil from '@/lib/utils/pathUtil';
+import { useGetPoint } from '@/service/Point/point.query';
+import { usePostBuyProduct } from '@/service/Shop/shop.mutation';
 import { useGetProductById } from '@/service/Shop/shop.query';
 import { MagnifyingGlassIcon } from '@public/icons/shared';
 import Image from 'next/image';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import React from 'react';
 
 const ShopDetail = () => {
+  const router = useRouter();
   const path = usePathname();
   const productId = PathUtil(path, 2);
 
   const [isOpenConfirmModal, setIsOpenConfirmModal] = React.useState(false);
 
   const { data, isLoading } = useGetProductById(String(productId));
-  const productData = data?.data;
+  const productData = data?.data?.product;
+  const availableStock = data?.data?.inventory?.availableStock;
 
-  if (isLoading) {
+  const { data: pointData, isLoading: isGetPointLoading } = useGetPoint();
+  const totalPoint = pointData?.data?.totalPoint;
+
+  const { mutate } = usePostBuyProduct(String(productId));
+  const handlePurchase = () => {
+    mutate(undefined, {
+      onSuccess: () => {
+        setIsOpenConfirmModal(false);
+        router.push(
+          `/point/shop/${productId}/success?image=${productData.imgURL}`
+        );
+      },
+    });
+  };
+
+  const isEnoughPoint = totalPoint - productData?.price >= 0;
+
+  if (isLoading || isGetPointLoading) {
     return <Loading />;
   }
 
@@ -58,28 +79,48 @@ const ShopDetail = () => {
         </div>
 
         <div className="fixed bottom-0 flex items-center justify-center w-full h-16 gap-4 px-6 pt-3 border-t bg-gray-white border-gray-50 custom601:mb-6">
-          <BottomButton
-            title="구매하기"
-            onClick={() => {
-              setIsOpenConfirmModal(true);
-            }}
-            disabled={'false'}
-          />
+          {availableStock > 0 ? (
+            <BottomButton
+              title="구매하기"
+              onClick={() => {
+                setIsOpenConfirmModal(true);
+              }}
+              disabled={'false'}
+            />
+          ) : (
+            <BottomButton
+              title="재고 없음"
+              onClick={() => {}}
+              disabled={'true'}
+            />
+          )}
         </div>
       </div>
 
-      {isOpenConfirmModal && (
-        <SelectModal
-          title="상품을 구매하시겠습니까?"
-          description="구매 후 취소가 불가능합니다."
-          cancel={() => {
-            setIsOpenConfirmModal(false);
-          }}
-          confirm={() => {}}
-          usePoint={2000}
-          totalPoint={5000}
-        />
-      )}
+      {isOpenConfirmModal ? (
+        isEnoughPoint ? (
+          <SelectModal
+            title="상품을 구매하시겠습니까?"
+            description="구매 후 취소가 불가능합니다."
+            cancel={() => {
+              setIsOpenConfirmModal(false);
+            }}
+            confirm={handlePurchase}
+            usePoint={productData?.price}
+            totalPoint={totalPoint - productData?.price}
+          />
+        ) : (
+          <SelectModal
+            title="상품을 구매할 수 없습니다."
+            description="포인트가 부족합니다."
+            cancel={() => {
+              setIsOpenConfirmModal(false);
+            }}
+            usePoint={productData?.price}
+            totalPoint={totalPoint - productData?.price}
+          />
+        )
+      ) : null}
     </div>
   );
 };
