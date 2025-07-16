e6c7bbdaf5465aeab756bd32610d8a4c94e1e2b2
Jamkris <dltmdgus1412@gmail.com>
2025-07-17 07:48:03 +0900

    feat(#83) :: MailIcon 추가

--- public/icons/Profile/index.ts
+++ public/icons/Profile/index.ts
@@ -3,3 +3,5 @@ export { default as CameraIcon } from './Camera';
 export { default as ShopIcon } from './Shop';
 export { default as PointIcon } from './Point';
 export { default as BookmarkIcon } from './Bookmark';
+export { default as NotificationIcon } from './Notification';
+export { default as MailIcon } from './Mail';
