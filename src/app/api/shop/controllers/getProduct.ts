644764e0f744917b477998f1e04499c9bdd11b53
Jamkris <dltmdgus1412@gmail.com>
2025-07-16 16:01:44 +0900

    feat(#79) :: 상품 카테고리로 조회되게 API 수정

--- src/app/api/shop/controllers/getProduct.ts
+++ src/app/api/shop/controllers/getProduct.ts
@@ -4,16 +4,43 @@ import resUtil from '@/lib/utils/responseUtil';
 import getUserFromRequest from '@/lib/utils/getUserFromRequest';
 import { Product, Users, Inventory } from '@/database/models';
 import { sequelize } from '@/database/sequelize';
+import { NextRequest } from 'next/server';
 
-async function postHandler() {
+async function postHandler(req: NextRequest) {
   try {
+    const { searchParams } = req.nextUrl;
+
+    const categoryParam = searchParams.get('category');
+
+    const categoryType = [
+      'cafe',
+      'pizza',
+      'chicken',
+      'dessert',
+      'burger',
+      'dessert',
+      'store',
+      'coupon',
+    ];
+
+    if (categoryParam && !categoryType.includes(categoryParam)) {
+      return resUtil.successTrue({
+        status: 400,
+        message:
+          '카테고리 타입이 잘못되었습니다. 타입은 cafe, pizza, chicken, dessert, burger, store, coupon 중 하나여야 합니다.',
+      });
+    }
+
     const user = await getUserFromRequest();
     const userRole = await Users.findOne({ where: { userId: user?.id } });
 
     let productData;
 
     if (userRole?.dataValues.role === 'user') {
+      const whereCondition = categoryParam ? { category: categoryParam } : {};
+
       productData = await Product.findAll({
+        where: whereCondition,
         include: [
           {
             model: Inventory,
@@ -44,7 +71,10 @@ async function postHandler() {
         group: ['Product.id'],
       });
     } else {
-      productData = await Product.findAll();
+      const whereCondition = categoryParam ? { category: categoryParam } : {};
+      productData = await Product.findAll({
+        where: whereCondition,
+      });
     }
 
     return resUtil.successTrue({
