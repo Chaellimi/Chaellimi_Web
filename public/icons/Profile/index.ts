5e9b6fa7818b6fd58ac0c6293d1e037854e60dfe
Jamkris <dltmdgus1412@gmail.com>
2025-07-17 07:40:57 +0900

    feat(#83) :: PointIcon 추가

--- public/icons/Profile/index.ts
+++ public/icons/Profile/index.ts
@@ -1,3 +1,4 @@
 export { default as AlarmIcon } from './Alarm';
 export { default as CameraIcon } from './Camera';
 export { default as ShopIcon } from './Shop';
+export { default as PointIcon } from './Point';
