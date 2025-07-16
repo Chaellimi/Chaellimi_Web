2e25def73cacc87ec599864ec24071f41bbcf09d
Jamkris <dltmdgus1412@gmail.com>
2025-07-16 20:42:59 +0900

    feat(#79) :: API 수정에 따라 Type 재선언

--- src/app/api/shop/Product.type.ts
+++ src/app/api/shop/Product.type.ts
@@ -7,3 +7,33 @@ export interface ProductType {
   title: string;
   explanation: string;
 }
+
+export interface CustodyType {
+  id: number;
+  productId: number;
+  inventoryId: number;
+  userId: number;
+  createdAt: string;
+  updatedAt: string;
+  product: {
+    id: number;
+    category: string;
+    imgURL: string;
+    brand: string;
+    price: string;
+    title: string;
+    explanation: string;
+    createdAt: string;
+    updatedAt: string;
+  };
+  inventory: {
+    id: number;
+    productId: number;
+    imgURL: string;
+    isSold: boolean;
+    isUse: boolean;
+    expiration: string;
+    createdAt: string;
+    updatedAt: string;
+  };
+}
