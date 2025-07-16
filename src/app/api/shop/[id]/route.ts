e0a73e38c3412c20ac81817a3b70f4eade907e3a
Jamkris <dltmdgus1412@gmail.com>
2025-07-16 16:46:49 +0900

    feat(#79) :: 상품 id로 조회 가능하게 수정

---
+++ src/app/api/shop/[id]/route.ts
@@ -0,0 +1,39 @@
+import { withAuth } from '@/lib/middleware/withAuth';
+import { withLogging } from '@/lib/middleware/withLogging';
+import { NextRequest } from 'next/server';
+import resUtil from '@/lib/utils/responseUtil';
+import { Product } from '@/database/models';
+
+async function getHandler(req: NextRequest) {
+  try {
+    const productId = Number(req.nextUrl.pathname.split('/')[3]);
+    if (!productId) {
+      return resUtil.successFalse({
+        status: 400,
+        message: '상품 ID가 제공되지 않았습니다',
+      });
+    }
+
+    const product = await Product.findOne({
+      where: { id: productId },
+    });
+
+    if (!product) {
+      return resUtil.successFalse({
+        status: 404,
+        message: '해당 상품을 찾을 수 없습니다',
+      });
+    }
+
+    return resUtil.successTrue({
+      status: 200,
+      message: '챌린지 조회 성공',
+      data: product,
+    });
+  } catch (err) {
+    return resUtil.unknownError({ data: { err } });
+  }
+}
+
+const GetProductById = withLogging(withAuth(getHandler));
+export const GET = GetProductById;
