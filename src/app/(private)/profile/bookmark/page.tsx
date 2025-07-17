c86fae58418073da256fd895bd0491325f052795
Jamkris <dltmdgus1412@gmail.com>
2025-07-17 14:47:37 +0900

    feat(#83) :: 북마크 해제 및 설정 가능하게 usePostBookmarkUpdate 훅 연결

--- src/app/(private)/profile/bookmark/page.tsx
+++ src/app/(private)/profile/bookmark/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
+import React, { useEffect, useState } from 'react';
 import ChallengeContent from '@/components/Challenge/ChallengeContent';
 import Header from '@/components/shared/Header';
 import Loading from '@/components/shared/Loading';
+import { usePostBookmarkUpdate } from '@/service/shared/shared.mutation';
 import { useGetBookmarkList } from '@/service/shared/shared.query';
-import React from 'react';
 
 interface BookmarkType {
   id: number;
@@ -35,43 +36,68 @@ interface BookmarkType {
 
 const Bookmark = () => {
   const { data, isLoading } = useGetBookmarkList();
-  const bookmarkList = data?.data;
-  console.log(bookmarkList);
+  const bookmarkList = React.useMemo(() => data?.data ?? [], [data]);
+
+  const { mutate } = usePostBookmarkUpdate();
+
+  const [checkedItems, setCheckedItems] = useState<Record<number, boolean>>({});
+
+  useEffect(() => {
+    if (bookmarkList.length > 0) {
+      const initialChecked = Object.fromEntries(
+        bookmarkList.map((item: BookmarkType) => [item.id, true])
+      );
+      setCheckedItems(initialChecked);
+    }
+  }, [bookmarkList]);
 
   if (isLoading) {
     return <Loading />;
   }
 
-  return (
-    <div>
-      <Header type="default" title="저장됨" />
+  const handleBookmarkClick = (challengeId: number, bookmarkId: number) => {
+    mutate(challengeId);
+    setCheckedItems((prev) => ({
+      ...prev,
+      [bookmarkId]: false,
+    }));
+  };
 
-      {bookmarkList?.length > 0 ? (
-        bookmarkList?.map((item: BookmarkType) => (
-          <div
-            className="flex-1 px-8 pb-16 mt-2 overflow-y-scroll scrollbar-hide -webkit-overflow-scrolling-touch overscroll-contain"
-            key={item.id}
-          >
-            <div className="grid grid-cols-2 gap-x-5 gap-y-5">
-              <ChallengeContent
-                id={item.id}
-                title={item.challenge.title}
-                imgUrl={item.challenge.imgURL}
-                days={Number(item.challenge.day)}
-                count={item.challenge.participantCount}
-                difficulty={item.challenge.difficulty}
-                createrName={item.challenge.User.name}
-                createrImgUrl={item.challenge.User.profileImg}
-                isChecked={true}
-                onClickBookmark={() => {}}
-                link={`/challenge/${item.challenge.id}?back=/profile/bookmark`}
-              />
+  return (
+    <div className="flex flex-col w-full h-full">
+      <Header type="default" title="저장됨" backClick="/profile" />
+
+      <div className="flex-1 px-4 pb-16 mt-2 overflow-y-scroll scrollbar-hide -webkit-overflow-scrolling-touch overscroll-contain">
+        <div className="grid grid-cols-2 gap-x-5 gap-y-5">
+          {bookmarkList.length > 0 ? (
+            bookmarkList.map(
+              (item: BookmarkType) =>
+                checkedItems[item.id] && (
+                  <ChallengeContent
+                    key={item.id}
+                    id={item.id}
+                    title={item.challenge.title}
+                    imgUrl={item.challenge.imgURL}
+                    days={Number(item.challenge.day)}
+                    count={item.challenge.participantCount}
+                    difficulty={item.challenge.difficulty}
+                    createrName={item.challenge.User.name}
+                    createrImgUrl={item.challenge.User.profileImg}
+                    isChecked={true}
+                    onClickBookmark={() =>
+                      handleBookmarkClick(item.challengeId, item.id)
+                    }
+                    link={`/challenge/${item.challenge.id}?back=/profile/bookmark`}
+                  />
+                )
+            )
+          ) : (
+            <div className="col-span-2 text-center text-gray-500">
+              저장된 챌린지가 없습니다.
             </div>
-          </div>
-        ))
-      ) : (
-        <div></div>
-      )}
+          )}
+        </div>
+      </div>
     </div>
   );
 };
