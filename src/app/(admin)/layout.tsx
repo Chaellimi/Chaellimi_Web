af183ff808b6038a3fb83640ae5bb31d07540307
Jamkris <dltmdgus1412@gmail.com>
2025-07-19 22:00:01 +0900

    chore(#83) :: 필요없는 코드 제거

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -4,7 +4,6 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import Loading from '@/components/shared/Loading';
-import { useGetUserRole } from '@/service/shared/shared.query';
 
 export default function AuthLayout({
   children,
@@ -14,9 +13,6 @@ export default function AuthLayout({
   const router = useRouter();
   const { data: myInfo, status } = useSession();
   const isLoading = status === 'loading';
-
-  const user = useGetUserRole();
-  console.log(user);
 
   useEffect(() => {
     if (!isLoading && !myInfo) {
@@ -28,5 +24,5 @@ export default function AuthLayout({
     return <Loading />;
   }
 
-  return <>{children}</>;
+  return <div>{children}</div>;
 }
